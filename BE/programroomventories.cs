92dacbd7587da0786a19a2cbf4141c36e7dc607d
Voimamut <tranduong12072005@gmail.com>
2026-03-18 18:46:09 +0700

    program
    thêm để kích hoạt lệnh

---
+++ BE/programroomventories.cs
@@ -0,0 +1,3 @@
+﻿// Đăng ký module RoomInventory
+builder.Services.AddScoped<IRoomInventoryRepository, RoomInventoryRepository>();
+builder.Services.AddScoped<IRoomInventoryService, RoomInventoryService>();
