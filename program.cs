e8d3830992c69aae24cf31fe5b05111e6dc4870f
Voimamut <tranduong12072005@gmail.com>
2026-03-18 18:36:11 +0700

    bổ sung
    chèn đoạn mã đăng ký sau (đặt trước builder.Build()

---
+++ program.cs
@@ -0,0 +1,7 @@
+﻿// --- Đăng ký Repository (Tầng dữ liệu) ---
+// Dùng Scoped vì mỗi request cần một phiên làm việc Database riêng
+builder.Services.AddScoped<IAttractionRepository, AttractionRepository>();
+
+// --- Đăng ký Service (Tầng logic) ---
+// Dùng Scoped để giải phóng bộ nhớ sau khi xử lý xong một Request
+builder.Services.AddScoped<IAttractionService, AttractionService>();
