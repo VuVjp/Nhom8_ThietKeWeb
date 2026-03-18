7b7f81fa61e77de706fee06336c4f509c41967b7
Voimamut <tranduong12072005@gmail.com>
2026-03-18 18:40:28 +0700

    thao tắc dtb

---
+++ BE/RepositoriesInterfacesIRoomInventoryRepository.cs
@@ -0,0 +1,16 @@
+using HotelManagement.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Repositories.Interfaces;
+
+public interface IRoomInventoryRepository
+{
+	Task<IEnumerable<RoomInventory>> GetAllAsync();
+	Task<IEnumerable<RoomInventory>> GetByRoomIdAsync(int roomId);
+	Task<RoomInventory?> GetByIdAsync(int id);
+	Task AddAsync(RoomInventory inventory);
+	void Update(RoomInventory inventory);
+	void Delete(RoomInventory inventory);
+	Task<bool> SaveChangesAsync();
+}
