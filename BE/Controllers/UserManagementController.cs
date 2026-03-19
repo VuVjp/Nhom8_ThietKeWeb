b095937d70c18ed1f98652d3fc1b02e3e18965dc
Nguyen Hoang Nguyen Vu <hoangnguyenvu157@gmail.com>
2026-03-19 19:46:05 +0700

    Update UserManagementController.cs

--- BE/Controllers/UserManagementController.cs
+++ BE/Controllers/UserManagementController.cs
@@ -25,27 +25,27 @@ public class UserManagementController : ControllerBase
     public async Task<IActionResult> CreateUser([FromBody] CreateUserDto dto)
     {
         await _userManagementService.CreateUserAsync(dto.Email, dto.Password, dto.RoleId);
-        return Ok();
+        return NoContent();
     }
 
     [HttpPut("{userId}")]
     public async Task<IActionResult> EditUser(int userId, [FromBody] EditUserDto dto)
     {
         await _userManagementService.EditUserAsync(userId, dto.Email, dto.Password, dto.RoleId);
-        return Ok();
+        return NoContent();
     }
 
     [HttpPut("{userId}/change-role")]
     public async Task<IActionResult> ChangeUserRole(int userId, [FromBody] ChangeRoleDto dto)
     {
         await _userManagementService.ChangeRoleByIdAsync(userId, dto.RoleId);
-        return Ok();
+        return NoContent();
     }
 
     [HttpDelete("{userId}")]
     public async Task<IActionResult> SoftDeleteUser(int userId)
     {
         await _userManagementService.SoftDeleteUserByIdAsync(userId);
-        return Ok();
+        return NoContent();
     }
 }
